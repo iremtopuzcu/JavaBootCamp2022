9e644ea670e7bab50294db7d539beda87b9c4d8c
iremtopuzcu <iremtopuzcu@hotmail.com>
2022-09-18 17:17:01 +0300

    week-1 homework

---
+++ week-1/conditions/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int sayi = 20;
+
+        if( sayi < 20){
+            System.out.println(" sayi 20'den kucuktur.");
+        }
+        else
+            System.out.println(" sayi 20'den kucuk degildir");
+
+        if (sayi < 15)
+            System.out.println(" sayi 15'ten kucuktur");
+
+        else{
+            System.out.println(" sayi 15'ten kucuk degildir");
+        }
+} }
