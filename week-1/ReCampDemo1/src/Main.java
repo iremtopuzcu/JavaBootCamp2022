a0c25a139abe54cae2f77a88f39ea0700ac0c283
iremtopuzcu <iremtopuzcu@hotmail.com>
2022-09-18 17:23:49 +0300

    week-1

--- week-1/ReCampDemo1/src/Main.java
+++ week-1/ReCampDemo1/src/Main.java
@@ -4,6 +4,15 @@ public class Main {
         int sayi1 = 20;
         int sayi2 = 25;
         int sayi3 = 2;
+        int enBuyuk = sayi1;
 
+        if ( enBuyuk < sayi2 ){
+            enBuyuk = sayi2;
+        }
+        if( enBuyuk < sayi3){
+            enBuyuk = sayi3
+        }
+
+        System.out.println("en buyuk = "+enBuyuk);
     }
 }
