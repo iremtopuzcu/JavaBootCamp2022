3ba0549f5e274056d1b45a441c61c07fb1b3ee0f
iremtopuzcu <90530305+iremtopuzcu@users.noreply.github.com>
2022-09-28 15:22:55 +0300

    Add files via upload

---
+++ week-2/variables/methods/src/methods/Main.java
@@ -0,0 +1,39 @@
+package methods;
+
+public class Main {
+
+	public static void main(String[] args) {
+		sayiBulmaca();
+		sayiBulmaca();
+		sayiBulmaca();
+
+	}
+	
+	public static void sayiBulmaca() {
+		int[] sayilar = new int[] {1,2,5,7,9,0};
+		int aranacak = 6;
+		boolean varMi = false;
+		
+		for(int sayi : sayilar) {
+			if(sayi==aranacak) {
+				varMi = true;
+				break;
+			}
+		}
+		
+		if(varMi) {
+			mesajVer(aranacak);
+		}
+		else {
+			System.out.println("Sayi mevcut degildir. "+aranacak);
+		}
+		
+	}
+	
+	public static void mesajVer(int aranacak) {
+		System.out.println("Sayi mevcut. "+aranacak);
+		
+		
+	}
+
+}
